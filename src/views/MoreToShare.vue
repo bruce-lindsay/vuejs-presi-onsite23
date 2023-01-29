0220852473fe17f88e5c6b759a0a9f70ec00d408
bruce@bendhsa.com <bruce@bendhsa.com>
2023-01-29 13:18:52 -0500

    include external link to boostrapvue

--- src/views/MoreToShare.vue
+++ src/views/MoreToShare.vue
@@ -4,7 +4,7 @@
     <ul>
       <li>Production example, HsaDebitCards.vue</li>
       <li>Component library</li>
-      <li>Bootstrap vue</li>
+      <li><a href="https://bootstrap-vue.org/docs/components">Bootstrap vue</a></li>
       <li>This presentation made with Vue </li>
     </ul>
     <a href="https://github.com/bruce-lindsay/vuejs-presi-onsite23">https://github.com/bruce-lindsay/vuejs-presi-onsite23</a>
